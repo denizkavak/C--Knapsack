60323313dfd18ba92f08132fac2a00d30aae482c
Deniz Kavak <denizzkavak@gmail.com>
2023-05-02 15:26:30 +0300

    knapsack problem
    For the knapsack problem C#:
    The first row and first column indicate the number of items, and the second column indicates the maximum capacity of the knapsack.
    For the other rows, the first column indicates the value of the item, and the second column indicates its weight.

---
+++ Knapsack/Knapsack/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Knapsack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 102);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(276, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(297, 36);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(240, 329);
+            this.listBox2.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 204);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(279, 160);
+            this.listBox3.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(427, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Evet/Hayır";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Dosya itemleri yazılsın mı?";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(40, 407);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(497, 192);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 192);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "K\r\nO\r\nP\r\nY\r\nA\r\nL\r\nA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 381);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "1.itemden başlayarak sıralı bir şekilde verilmiştir.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 611);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "19 ve 200 Boyutlu Dosyalar İçin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Knapsack/Knapsack/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Knapsack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string line;
+        int linecount = 0;
+        int i = 0, j = 0;
+        int satirSayisi;
+        int sutunSayisi;
+        int itemSayısı = 0;
+        int maxKapasite = 0;
+        int[,] dizi;
+        int[,] data;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                listBox3.Items.Clear();
+                OpenFileDialog file = new OpenFileDialog();
+                file.RestoreDirectory = true;
+                file.CheckFileExists = false;
+                file.Title = "Dosyayı seçiniz...";
+                file.ShowDialog();
+
+                string DosyaYolu = file.FileName;
+                string DosyaAdi = file.SafeFileName;
+                label1.Text = DosyaAdi;
+                label2.Text = DosyaYolu;
+
+                FileStream dosya = new FileStream(DosyaYolu, FileMode.Open, FileAccess.Read);
+                StreamReader sw = new StreamReader(dosya);
+                string yazi = sw.ReadLine();
+
+                //satır ve sütun sayısı hesaplandı
+                string[] veri = yazi.Split(' ');
+                string[] satirlar = File.ReadAllLines(DosyaYolu);
+                satirSayisi = satirlar.Length;
+                sutunSayisi = satirlar[0].Split(' ').Length;
+
+                richTextBox1.Text = "Satır sayısı: " + satirSayisi.ToString();
+                richTextBox1.Text += "\nSütun sayısı:" + sutunSayisi;
+
+                dizi = new int[satirSayisi, sutunSayisi];
+                data = new int[satirSayisi, sutunSayisi];
+                using (StreamReader reader = new StreamReader(DosyaYolu))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] deger = line.Split(' ');
+                        if (linecount == 0)
+                        {
+                            itemSayısı = int.Parse(deger[0]);
+                            maxKapasite = int.Parse(deger[1]);
+                            dizi[0, 0] = itemSayısı;
+                            dizi[0, 1] = maxKapasite;
+                            richTextBox1.Text += "\nToplam item sayısı: " + itemSayısı.ToString();
+                            richTextBox1.Text += "\nMax çanta ağırlığı:" + maxKapasite.ToString();
+
+                        }
+                        else
+                        {
+                            i++;
+                            if (i == itemSayısı + 1)
+                            {
+                                break;
+                            }
+                            j = 0;
+                            dizi[i, j] = int.Parse(deger[0]);
+                            j = 1;
+                            dizi[i, j] = int.Parse(deger[1]);
+                        }
+                        linecount++;
+                    }
+                }
+                dosya.Close();
+                linecount = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("BELİRLENEN FORMATTA BİR DOSYA SEÇMENİZ LAZIM!!!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                for (int k = 1; k <= itemSayısı; k++)
+                {
+                    listBox2.Items.Add(k.ToString() + ".--> Value: " + dizi[k, 0].ToString() + " / " + k.ToString() + ". Weight: " + dizi[k, 1].ToString());
+                }
+            }
+            else
+            {
+                listBox2.Items.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string yazi = " ";
+                string tyazi = " ";
+                richTextBox2.Text = " ";
+                int cap = 0;
+                int[,] canta = new int[itemSayısı + 1, maxKapasite + 1];
+                cap = maxKapasite;
+                //Hesaplamanın yapıldığı kod satırı
+                for (int item = 1; item <= itemSayısı; item++)
+                {
+                    for (int capacity = 1; capacity <= maxKapasite; capacity++)
+                    {
+                        if (item == 0 || capacity == 0)
+                        {
+                            canta[item, capacity] = 0;
+                        }
+                        else if (dizi[item, 1] <= capacity)
+                        {
+                            canta[item, capacity] = Math.Max(
+                                dizi[item, 0] + canta[item - 1, capacity - dizi[item, 1]],
+                                                  canta[item - 1, capacity]
+                                                  );
+                        }
+                        else
+                        {
+                            canta[item, capacity] = canta[item - 1, capacity];
+                        }
+                    }
+                }
+                //Hangi verilerin kullanıldığını gösteren kod satırları
+                listBox3.Items.Add("Kapladığı yer: " + canta[itemSayısı, maxKapasite]);
+                int m = itemSayısı;
+                int n = maxKapasite;
+                listBox3.Items.Add("Kullanılan itemler(0-Kullanılmadı/1-Kullanıldı)");
+                while (m > 0 && n > 0)
+                {
+                    if (canta[m, n] == canta[m - 1, n])
+                    {
+                        //item excluded
+                        listBox3.Items.Add(m + ". İtem --> 0");
+                        yazi += " 0";
+                    }
+                    else
+                    {
+                        //item included
+                        listBox3.Items.Add(m + ". İtem--> 1");
+                        yazi += " 1";
+
+                        n = n - dizi[m, 1];
+                    }
+                    m--;
+                }
+                for (int u = yazi.Length - 1; u >= 0; u--)
+                {
+                    tyazi = tyazi + yazi[u].ToString();
+                }
+                richTextBox2.Text += tyazi;
+
+
+            }
+            catch (Exception ett)
+            {
+                MessageBox.Show(ett.ToString());
+            }
+        }
+    }
+}
